4c328ed71503ca094e44d2bb7519f52164bed1c2
Ran Kim <rannio@gmail.com>
2022-03-28 12:20:42 -0400

    created seed documents for user collection and seeded documents

---
+++ seed/users.js
@@ -0,0 +1,32 @@
+///////////////////// IMPORT ////////////////////////////////////
+
+const db = require("../db");
+const { User } = require("../models");
+
+// Connect to the database
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+/////////////// CREATED REVIEW SEED TO ADD TO REVIEW COLLECTION /////////////////////
+const main = async () => {
+  const users = [
+    {
+        name: 'Ran Kim',
+        email: 'ranfakeemail@gmail.com',
+        password: '123123rankim'
+    },
+    {
+        name: 'San Kim',
+        email: 'sanfakeemail@gmail.com',
+        password: '123123dankim'
+    }
+  ]
+    
+  await User.insertMany(users);
+  console.log("Created some users!");
+};
+const run = async () => {
+  await main();
+  db.close();
+};
+
+run();
--- server.js
+++ server.js
@@ -1,7 +1,29 @@
+////////////// IMPORT ////////////////////
 const express = require('express')
-const PORT = process.env.PORT || 3001
+const cors = require("cors");
+const db = require("./db");
+const { Entry } = require("./models");
+const logger = require("morgan");
 
+//////// DEFINE VARIABLES /////////////
+const PORT = process.env.PORT || 3001
 const app = express()
+////////// MIDDLEWARE /////////////////////
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+
+/////////// ROUTES //////////////////////
+app.get("/", (req, res) => {
+  res.send("This is root!");
+});
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
